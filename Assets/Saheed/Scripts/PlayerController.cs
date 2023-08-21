ac010219f8e6e2e67da3de1194bf9c1a630c4a9a
Saheed ⭐ <65962444+SaheedKazeem@users.noreply.github.com>
2023-08-21 13:17:33 +0100

    Added jumping

--- Assets/Saheed/Scripts/PlayerController.cs
+++ Assets/Saheed/Scripts/PlayerController.cs
@@ -6,7 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     float XInput, YInput, Speed;
     Rigidbody2D RB;
+    [Header("Jump")]
+    [Range(1, 10)]
+    public float JumpVelocity;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    bool isJumping, isGrounded, CanDJump, isDJumping;
     // Start is called before the first frame update
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Ground")
+        {
+            //anim.SetBool("isGrounded", true);
+            isGrounded = true;
+            CanDJump = true;
+           // anim.ResetTrigger("isDJumping");
+        }
+
+    }
     void Start()
     {
         RB = GetComponent<Rigidbody2D>();
@@ -29,7 +47,7 @@ public class PlayerController : MonoBehaviour
         //Input
         XInput = Input.GetAxisRaw("Horizontal");
         YInput = Input.GetAxisRaw("Vertical");
-        
+        Jump();
        
         //Flipping Sprite
 
@@ -43,7 +61,51 @@ public class PlayerController : MonoBehaviour
         }
 
     }
-    
+      void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded == true)
+            {
+                RB.velocity = Vector2.up * JumpVelocity;
+                isJumping = true;
+                isGrounded = false;
+                //anim.SetBool("isGrounded", false);
+
+            }
+            else if (!isGrounded && CanDJump == true)
+            {
+                RB.velocity = Vector2.up * JumpVelocity;
+                CanDJump = false;
+               // anim.SetTrigger("isDJumping");
+
+            }
+        }
+        else isJumping = false;
+        if (isJumping == true)
+        {
+           // anim.SetBool("isJumping", true);
+        }
+        if (isJumping == false)
+        {
+           // anim.SetBool("isJumping", false);
+        }
+
+        BetterJump();
+        void BetterJump()
+    {
+        //Adds Weight and feel to the character's Jump <<<
+        if (RB.velocity.y < 0)
+        {
+            RB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (RB.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            RB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    }
 
 }
 
