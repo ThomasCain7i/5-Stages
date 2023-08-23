24d1bf9c3bdb5d1c2efbbf599adafc26786662dd
Thomas Cain <115077729+ThomasCain7i@users.noreply.github.com>
2023-08-23 21:35:41 +0100

    Audio For Denial and Menu added

---
+++ Assets/Thomas/Scripts/Audio/ThemeManager.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+public class ThemeManager : MonoBehaviour
+{
+    [Header("What Level")]
+    public bool menu;
+    public bool denial, anger, bargaining, depression, acceptance;
+
+    [Header("Audio Clips")]
+
+    [Header("Theme Music")]
+    public AudioClip[] menuTheme;
+    public AudioClip[] denialTheme;
+    public AudioClip[] angerTheme;
+    public AudioClip[] bargainingTheme;
+    public AudioClip[] depressionTheme;
+    public AudioClip[] acceptanceTheme;
+
+    [Header("Volume")]
+    [SerializeField]
+    private float VolumeMenu;
+    [SerializeField]
+    private float VolumeDenial, VolumeAnger, VolumeBargaining, VolumeDepression, VolumeAcceptance;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        PlayThemeMusic();
+    }
+
+    public void PlayThemeMusic()
+    {
+        if (menuTheme.Length > 0 && menu == true)
+        {
+            int randomIndex = Random.Range(0, menuTheme.Length);
+            audioSource.clip = menuTheme[randomIndex];
+            audioSource.volume = VolumeMenu;
+            audioSource.loop = true;
+            audioSource.Play();
+            Debug.Log("Play theme");
+        }
+
+        if (denialTheme.Length > 0 && denial == true)
+        {
+            int randomIndex = Random.Range(0, denialTheme.Length);
+            audioSource.clip = denialTheme[randomIndex];
+            audioSource.volume = VolumeDenial;
+            audioSource.loop = true;
+            audioSource.Play();
+            Debug.Log("Play theme");
+        }
+
+        if (angerTheme.Length > 0 && anger == true)
+        {
+            int randomIndex = Random.Range(0, angerTheme.Length);
+            audioSource.clip = angerTheme[randomIndex];
+            audioSource.volume = VolumeAnger;
+            audioSource.loop = true;
+            audioSource.Play();
+            Debug.Log("Play theme");
+        }
+
+        if (bargainingTheme.Length > 0 && bargaining == true)
+        {
+            int randomIndex = Random.Range(0, bargainingTheme.Length);
+            audioSource.clip = bargainingTheme[randomIndex];
+            audioSource.volume = VolumeBargaining;
+            audioSource.loop = true;
+            audioSource.Play();
+            Debug.Log("Play theme");
+        }
+
+        if (depressionTheme.Length > 0 && depression == true)
+        {
+            int randomIndex = Random.Range(0, depressionTheme.Length);
+            audioSource.clip = depressionTheme[randomIndex];
+            audioSource.volume = VolumeDepression;
+            audioSource.loop = true;
+            audioSource.Play();
+            Debug.Log("Play theme");
+        }
+
+        if (acceptanceTheme.Length > 0 && acceptance == true)
+        {
+            int randomIndex = Random.Range(0, acceptanceTheme.Length);
+            audioSource.clip = acceptanceTheme[randomIndex];
+            audioSource.volume = VolumeAcceptance;
+            audioSource.loop = true;
+            audioSource.Play();
+            Debug.Log("Play theme");
+        }
+    }
+}
