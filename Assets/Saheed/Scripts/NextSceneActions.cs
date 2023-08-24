1d8ab8ec65473953a1271d65dfbf150e5414834d
Thomas Cain <115077729+ThomasCain7i@users.noreply.github.com>
2023-08-24 17:54:00 +0100

    Depression Started

--- Assets/Saheed/Scripts/NextSceneActions.cs
+++ Assets/Saheed/Scripts/NextSceneActions.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class NextSceneActions : MonoBehaviour
 {
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            gameManager.SavePolaroid();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
--- Assets/Saheed/Scripts/PlayerController.cs
+++ Assets/Saheed/Scripts/PlayerController.cs
@@ -39,6 +39,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private GameObject PauseMenu;
     PauseMenu refToPauseMenuScript;
+    [SerializeField]
     GameManager gameManager;
 
 
